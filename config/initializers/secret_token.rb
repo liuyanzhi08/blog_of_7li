7228c128350023741eda79935fab61ecc8cb62fb
7LI <77svli@gmail.com>
2012-07-25 09:30:00 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogOf7li::Application.config.secret_token = '9435f9861e886d4d6d2076b1878737a07a4c8cd833c773954f8859b1bb0daaa33f868c74b2134f9d027bcacb35b4e996f3e5a188eb6f60dbf2921127ee79d71d'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+BlogOf7li::Application.routes.draw do
+  root :to => "home#index"
+end
