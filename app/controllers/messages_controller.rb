4601555e07c7fecb90e7a37df2be3c259844e28c
7LI <77svli@gmail.com>
2012-08-03 00:28:55 +0800

    fix leave a message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,10 @@
 class MessagesController < ApplicationController
-  load_and_authorize_resource #cancan use to authorize
   def index
     @message = Message.new
     @messages = Message.all
   end
 
   def create
-    authorize! :create, Message
     @message = Message.new(params[:message])
     @user = User.find_by_email_and_admin("77svli@gmail.com",true)
     @user.messages << @message
@@ -20,6 +18,7 @@ class MessagesController < ApplicationController
   end
 
   def destroy
+    authorize! :delete, Message
     @message = Message.find(params[:id])
     if @message.destroy
       redirect_to messages_url, notice: "Comment deleted"
